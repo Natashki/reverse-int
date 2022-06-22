c1517c1aae1f50598d56635f6322e2e9314740a6
Natashki <natsuchan42@gmail.com>
2022-06-22 08:12:47 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+    let m = Math.abs(n);
+    let s = m.toString();
+    let rever = '';
+    for(let i = 1; i<=s.length; i++){
+        rever = rever + s[s.length-i];
+    }
+    return rever;
 }
+// let n = -123;
+
+// console.log(rever);
